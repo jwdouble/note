89c933218a6bb2394c5f09e70d13275b349f52f2
jwdouble <954649468@qq.com>
2022-05-24 11:10:04 +0800

    refactor: config

--- conf/conf_test.go
+++ conf/conf_test.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"arctron.lib/conf"
@@ -13,15 +14,14 @@ func Test_conf(t *testing.T) {
 	fmt.Println(conf.APP_PG_DSN.Value(dsnDefault))
 }
 
-func Test_Viper(t *testing.T) {
+func Test_ViperConf(t *testing.T) {
 	// 方法1 直接设置文件详情
-	viper.SetConfigFile("../config.yaml")
+	//viper.SetConfigFile("../config.yaml")
 
 	// 方法2 设置文件名称（不带类型），设置文件类型，添加文件路径
-	//viper.SetConfigName("config") // name of config file (without extension)
-	//viper.SetConfigType("yaml")	// REQUIRED if the config file does not have the extension in the name
-	//viper.AddConfigPath("..")	// optionally look for config in the working directory
-
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("..")     // optionally look for config in the working directory
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
@@ -29,4 +29,14 @@ func Test_Viper(t *testing.T) {
 	r2 := viper.Get("app.engine.log")
 	r3 := viper.Get("test")
 	fmt.Println(r2, r3)
+}
+
+func Test_viperEnv(t *testing.T) {
+	viper.SetEnvPrefix("k8s") // will be uppercased automatically
+	viper.BindEnv("ID2")
+
+	os.Setenv("K8S_ID2", "13003") // typically done outside of the app
+
+	id := viper.GetString("ID2") // 13
+	fmt.Println(id)
 }
